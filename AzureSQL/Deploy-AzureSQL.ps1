82edafef68884701d5fb3f55c5f6bec731eba2a8
nenad.stojanovic <nenad.stojanovic@outlook.com>
2017-06-30 09:59:55 +0200

    SQL and Kubernetes

---
+++ AzureSQL/Deploy-AzureSQL.ps1
@@ -0,0 +1,18 @@
+﻿Login-AzureRmAccount
+
+$location = 'westeurope'
+$rgName = 'AzureSQL'
+   
+
+New-AzureRmResourceGroup -Name $rgName -Location $location
+
+Test-AzureRmResourceGroupDeployment -ResourceGroupName $rgName `
+                                   -TemplateFile .\azuresql.json `
+                                   -TemplateParameterFile .\azuresqlparameters.json `
+                                   -Verbose
+
+New-AzureRmResourceGroupDeployment -ResourceGroupName $rgName `
+                                   -TemplateFile .\azuresql.json `
+                                   -TemplateParameterFile .\azuresqlparameters.json `
+                                   -Verbose
+
